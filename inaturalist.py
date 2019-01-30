8be9927ef9c042c769594cdd32d78534d515ef93
Florin Gogianu <florin.gogianu@gmail.com>
2019-01-30 14:23:43 +0200

    Move the argparser and save the configuration on disk.

--- inaturalist.py
+++ inaturalist.py
@@ -9,6 +9,7 @@ import time
 import datetime
 import warnings
 import sys
+import yaml
 
 import torch
 import torch.nn as nn
@@ -22,153 +23,18 @@ import torch.utils.data.distributed
 import torchvision.models as models
 
 from datasets import iNaturalist
-from utils import FPSMetric, AverageMetric, Logger
+from utils import FPSMetric, AverageMetric, Logger, parse_cmd
 
-
-model_names = sorted(
-    name
-    for name in models.__dict__
-    if name.islower()
-    and not name.startswith("__")
-    and callable(models.__dict__[name])
-)
-
-parser = argparse.ArgumentParser(description="PyTorch ImageNet Training")
-parser.add_argument("data", metavar="DIR", help="path to dataset")
-parser.add_argument(
-    "-a",
-    "--arch",
-    metavar="ARCH",
-    default="resnet18",
-    choices=model_names,
-    help="model architecture: "
-    + " | ".join(model_names)
-    + " (default: resnet18)",
-)
-parser.add_argument(
-    "-j",
-    "--workers",
-    default=4,
-    type=int,
-    metavar="N",
-    help="number of data loading workers (default: 4)",
-)
-parser.add_argument(
-    "--epochs",
-    default=90,
-    type=int,
-    metavar="N",
-    help="number of total epochs to run",
-)
-parser.add_argument(
-    "--start-epoch",
-    default=0,
-    type=int,
-    metavar="N",
-    help="manual epoch number (useful on restarts)",
-)
-parser.add_argument(
-    "-b",
-    "--batch-size",
-    default=256,
-    type=int,
-    metavar="N",
-    help="mini-batch size (default: 256), this is the total "
-    "batch size of all GPUs on the current node when "
-    "using Data Parallel or Distributed Data Parallel",
-)
-parser.add_argument(
-    "--lr",
-    "--learning-rate",
-    default=0.1,
-    type=float,
-    metavar="LR",
-    help="initial learning rate",
-    dest="lr",
-)
-parser.add_argument(
-    "--momentum", default=0.9, type=float, metavar="M", help="momentum"
-)
-parser.add_argument(
-    "--wd",
-    "--weight-decay",
-    default=1e-4,
-    type=float,
-    metavar="W",
-    help="weight decay (default: 1e-4)",
-    dest="weight_decay",
-)
-parser.add_argument(
-    "-p",
-    "--print-freq",
-    default=10,
-    type=int,
-    metavar="N",
-    help="print frequency (default: 10)",
-)
-parser.add_argument(
-    "--save-log-freq",
-    default=200,
-    type=int,
-    metavar="N",
-    help="print frequency (default: 10)",
-)
-parser.add_argument(
-    "--resume",
-    default="",
-    type=str,
-    metavar="PATH",
-    help="path to latest checkpoint (default: none)",
-)
-parser.add_argument(
-    "-e",
-    "--evaluate",
-    dest="evaluate",
-    action="store_true",
-    help="evaluate model on validation set",
-)
-parser.add_argument(
-    "--pretrained",
-    dest="pretrained",
-    action="store_true",
-    help="use pre-trained model",
-)
-parser.add_argument(
-    "--world-size",
-    default=-1,
-    type=int,
-    help="number of nodes for distributed training",
-)
-parser.add_argument(
-    "--rank", default=-1, type=int, help="node rank for distributed training"
-)
-parser.add_argument(
-    "--dist-url",
-    default="tcp://224.66.41.62:23456",
-    type=str,
-    help="url used to set up distributed training",
-)
-parser.add_argument(
-    "--dist-backend", default="nccl", type=str, help="distributed backend"
-)
-parser.add_argument(
-    "--seed", default=None, type=int, help="seed for initializing training. "
-)
-parser.add_argument("--gpu", default=None, type=int, help="GPU id to use.")
-parser.add_argument(
-    "--multiprocessing-distributed",
-    action="store_true",
-    help="Use multi-processing distributed training to launch "
-    "N processes per node, which has N GPUs. This is the "
-    "fastest way to use PyTorch for either single node or "
-    "multi node data parallel training",
-)
 
 best_acc1 = 0
 
 
 def main():
-    args = parser.parse_args()
+    args = parse_cmd()
+
+    # create a run label
+    timestamp = f"{datetime.datetime.now():%Y%b%d-%H%M%S}"
+    args.run_label = f"{timestamp}_inat_{args.arch}"
 
     if args.seed is not None:
         random.seed(args.seed)
@@ -352,9 +218,7 @@ def main_worker(gpu, ngpus_per_node, args):
         return
 
     # configure logger
-    exp_name = f"{datetime.datetime.now():%Y%b%d-%H%M%S}_inat"
-
-    train_log = Logger(label="train", path=f"./results/{exp_name}")
+    train_log = Logger(label="train", path=f"./results/{args.run_label}")
     train_log.add_metrics(
         batch_time=AverageMetric(),
         data_time=AverageMetric(),
@@ -363,6 +227,11 @@ def main_worker(gpu, ngpus_per_node, args):
         top1=AverageMetric(),
         top5=AverageMetric(),
     )
+
+    # save the current options
+    with open(f'./results/{args.run_label}/cfg.yaml', 'w') as cfg:
+        yaml.dump(args.__dict__, cfg, default_flow_style=False)
+
 
     print("MAIN: starting training.")
 
@@ -443,11 +312,13 @@ def train(train_loader, model, criterion, optimizer, epoch, log, args):
         if i % args.print_freq == 0:
             # stdout stats
             log.log(cb=stdout_cb)
-            # save to disk
+
         if i % args.save_log_freq == 0:
+            # save to disk
             log.save()
 
         end = time.time()
+
     log.save()
 
 
